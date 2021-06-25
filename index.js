6f8ec161bb7a4d5e186bf65d9740d8fa41a48ddc
Christine-N-Mwaura <njerimwaura25@gmail.com>
2021-06-25 09:21:07 +0300

    add the javascript file which adds functionality to the website.

---
+++ index.js
@@ -0,0 +1,17 @@
+var pizzaCost = document.getElementById("pizzaOptions")
+
+function buyPizza() {
+    
+
+    var pizzaCostInput = document.getElementById("pizzaOptions")
+    var pizzaCost = pizzaCostInput.value
+    var numberOfPizzasInput = document.getElementById("numberOfPizzasInput")
+    var numberOfPizzas = numberOfPizzasInput.value
+    alert("buy " + numberOfPizzas + " " +" pizza(s) " + " each worth "+ pizzaCost )
+    alert("the total cost is " + numberOfPizzas * pizzaCost)
+    confirm("would you like to continue to purchase")
+    alert("Purchase successful. ")  
+    
+}
+
+buyPizza()
